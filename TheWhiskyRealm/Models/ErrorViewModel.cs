bf3226c1a22f1c961f019b6ae6f83fca52d862a0
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-03-17 19:56:35 +0200

    Add project files.

---
+++ TheWhiskyRealm/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TheWhiskyRealm.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
