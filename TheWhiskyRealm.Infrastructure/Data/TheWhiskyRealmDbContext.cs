e021aaed83a9b37195bb890b3e1d07c7c2734c38
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-03-17 20:09:40 +0200

    Initial Project Architecture

---
+++ TheWhiskyRealm.Infrastructure/Data/TheWhiskyRealmDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TheWhiskyRealm.Infrastructure.Data;
+
+public class TheWhiskyRealmDbContext : IdentityDbContext
+{
+    public TheWhiskyRealmDbContext(DbContextOptions<TheWhiskyRealmDbContext> options)
+        : base(options)
+    {
+
+    }
+}
--- TheWhiskyRealm/Program.cs
+++ TheWhiskyRealm/Program.cs
@@ -1,17 +1,17 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using TheWhiskyRealm.Data;
+using TheWhiskyRealm.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+builder.Services.AddDbContext<TheWhiskyRealmDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddEntityFrameworkStores<TheWhiskyRealmDbContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
