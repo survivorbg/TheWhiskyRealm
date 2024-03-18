d370d311f4c5d2259367e84623eb36a95dbe537d
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-03-18 20:43:37 +0200

    Distillery entity added

---
+++ TheWhiskyRealm.Infrastructure/Constants/DistilleryDataConstants.cs
@@ -0,0 +1,27 @@
+﻿namespace TheWhiskyRealm.Infrastructure.Constants;
+
+/// <summary>
+/// Data Constants for the entity Distillery.
+/// </summary>
+public static class DistilleryDataConstants
+{
+    /// <summary>
+    /// Minimum distillery name length.
+    /// </summary>
+    public const int MinNameLength = 3;
+
+    /// <summary>
+    /// Maximum distillery name length.
+    /// </summary>
+    public const int MaxNameLength = 57;
+
+    /// <summary>
+    /// Minimum value for the year that distillery is founded.
+    /// </summary>
+    public const double MinYearFounded = 1690;
+
+    /// <summary>
+    /// Maximum value for the year that distillery is founded.
+    /// </summary>
+    public const double MaxYearFounded = 2024;
+}
