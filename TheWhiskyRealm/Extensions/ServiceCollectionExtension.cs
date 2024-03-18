b673af7ac4a036dc6a36e92fff22aa700ba44d01
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-03-18 17:42:35 +0200

    IServiceCollection extended.

---
+++ TheWhiskyRealm/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TheWhiskyRealm.Infrastructure.Data;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddApplicationService(this IServiceCollection services)
+    {
+        return services;
+    }
+
+    public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
+    {
+        var connectionString = config.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<TheWhiskyRealmDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
+        services.AddDatabaseDeveloperPageExceptionFilter();
+
+        return services;
+    }
+
+    public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddDefaultIdentity<IdentityUser>(options =>
+            {
+
+            })
+            .AddEntityFrameworkStores<TheWhiskyRealmDbContext>();
+
+        return services;
+    }
+}
--- TheWhiskyRealm/Program.cs
+++ TheWhiskyRealm/Program.cs
@@ -1,22 +1,12 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using TheWhiskyRealm.Infrastructure.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<TheWhiskyRealmDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddApplicationDbContext(builder.Configuration);
+builder.Services.AddApplicationIdentity(builder.Configuration);
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<TheWhiskyRealmDbContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -24,7 +14,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
