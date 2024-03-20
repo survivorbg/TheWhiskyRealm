925e0480b258ade590515df4c81864633eb66b5e
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-03-21 00:08:05 +0200

    Seeded Regions

---
+++ TheWhiskyRealm.Infrastructure/Data/Configurations/RegionConfiguration.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TheWhiskyRealm.Infrastructure.Data.Models;
+
+namespace TheWhiskyRealm.Infrastructure.Data.Configurations;
+
+public class RegionConfiguration : IEntityTypeConfiguration<Region>
+{
+    public void Configure(EntityTypeBuilder<Region> builder)
+    {
+        builder.HasData(GenerateRegions());
+    }
+    private Region[] GenerateRegions()
+    {
+        ICollection<Region> regions = new HashSet<Region>();
+
+        regions.Add(new Region
+        {
+            Id = 1,
+            Name = "Lowland",
+            CountryId = 1,
+        });
+
+        regions.Add(new Region
+        {
+            Id = 2,
+            Name = "Highland",
+            CountryId = 1,
+        });
+
+        regions.Add(new Region
+        {
+            Id = 3,
+            Name = "Speyside",
+            CountryId = 1,
+        });
+
+        regions.Add(new Region
+        {
+            Id = 4,
+            Name = "Islands",
+            CountryId = 1,
+        });
+
+        regions.Add(new Region
+        {
+            Id = 5,
+            Name = "Campbeltown",
+            CountryId = 1,
+        });
+
+        regions.Add(new Region
+        {
+            Id = 6,
+            Name = "Islay",
+            CountryId = 1,
+        });
+
+        regions.Add(new Region
+        {
+            Id = 7,
+            Name = "Antrim County",
+            CountryId = 2,
+        });
+
+        regions.Add(new Region
+        {
+            Id = 8,
+            Name = "Cork County ",
+            CountryId = 2,
+        });
+
+        regions.Add(new Region
+        {
+            Id = 9,
+            Name = "Dublin County ",
+            CountryId = 2,
+        });
+        
+        regions.Add(new Region
+        {
+            Id = 10,
+            Name = "Offaly County ",
+            CountryId = 2,
+        });
+        regions.Add(new Region
+        {
+            Id = 11,
+            Name = "Kentucky",
+            CountryId = 3,
+        });
+
+        regions.Add(new Region
+        {
+            Id = 12,
+            Name = "Tennessee",
+            CountryId = 3,
+        });
+
+        regions.Add(new Region
+        {
+            Id = 13,
+            Name = "Osaka Prefecture",
+            CountryId = 4,
+        });
+
+        regions.Add(new Region
+        {
+            Id = 14,
+            Name = "Yilan County",
+            CountryId = 5,
+        });
+
+        regions.Add(new Region
+        {
+            Id = 15,
+            Name = "Karnataka",
+            CountryId = 6,
+        });
+
+        regions.Add(new Region
+        {
+            Id = 16,
+            Name = "Ontario",
+            CountryId = 7,
+        });
+
+        return regions.ToArray();
+    }
+}
---
+++ TheWhiskyRealm.Infrastructure/Data/Migrations/20240320220712_RegionSeed.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheWhiskyRealm.Infrastructure.Data
+{
+    public partial class RegionSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Regions",
+                columns: new[] { "Id", "CountryId", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Lowland" },
+                    { 2, 1, "Highland" },
+                    { 3, 1, "Speyside" },
+                    { 4, 1, "Islands" },
+                    { 5, 1, "Campbeltown" },
+                    { 6, 1, "Islay" },
+                    { 7, 2, "Antrim County" },
+                    { 8, 2, "Cork County " },
+                    { 9, 2, "Dublin County " },
+                    { 10, 2, "Offaly County " },
+                    { 11, 3, "Kentucky" },
+                    { 12, 3, "Tennessee" },
+                    { 13, 4, "Osaka Prefecture" },
+                    { 14, 5, "Yilan County" },
+                    { 15, 6, "Karnataka" },
+                    { 16, 7, "Ontario" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: 16);
+        }
+    }
+}
