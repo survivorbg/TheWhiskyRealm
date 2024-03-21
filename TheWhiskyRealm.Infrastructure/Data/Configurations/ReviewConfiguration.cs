1a199626f7d249774bc893706ca22a15bf167068
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-03-21 07:43:23 +0200

    Seeded Reviews

---
+++ TheWhiskyRealm.Infrastructure/Data/Configurations/ReviewConfiguration.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TheWhiskyRealm.Infrastructure.Data.Models;
+
+namespace TheWhiskyRealm.Infrastructure.Data.Configurations
+{
+    public class ReviewConfiguration : IEntityTypeConfiguration<Review>
+    {
+        public void Configure(EntityTypeBuilder<Review> builder)
+        {
+            builder.HasData(GenerateReviews());
+        }
+
+        private Review[] GenerateReviews()
+        {
+            ICollection<Review> reviews = new List<Review>();
+
+            reviews.Add(new Review
+            {
+                Id = 1,
+                Title = "Fantastic flavor!",
+                Content = "This whisky has an amazing taste profile, rich and complex.",
+                Recommend = true,
+                WhiskyId = 1,
+                UserId = "a8909756-a101-47c5-8d52-085322ffa6e6"
+            });
+
+            reviews.Add(new Review
+            {
+                Id = 2,
+                Title = "Smooth and enjoyable",
+                Content = "Really enjoyed sipping on this whisky, smooth with a nice finish.",
+                Recommend = true,
+                WhiskyId = 2,
+                UserId = "bca5356a-d5d8-47d7-b314-e74901211b99"
+            });
+
+            reviews.Add(new Review
+            {
+                Id = 3,
+                Title = "Great!",
+                Content = "This whisky is unbeatable. Smooth and easy to drink.",
+                Recommend = true,
+                WhiskyId = 3,
+                UserId = "d9ef08e4-4307-4a78-8b8c-afaea5d8a0d2"
+            });
+
+            reviews.Add(new Review
+            {
+                Id = 4,
+                Title = "Disappointing",
+                Content = "Expected more from this whisky. Found it lacking in flavor.",
+                Recommend = false,
+                WhiskyId = 4,
+                UserId = "a8909756-a101-47c5-8d52-085322ffa6e6"
+            });
+
+            reviews.Add(new Review
+            {
+                Id = 5,
+                Title = "A real treat",
+                Content = "This whisky is a real treat for the senses. Highly recommended.",
+                Recommend = true,
+                WhiskyId = 5,
+                UserId = "bca5356a-d5d8-47d7-b314-e74901211b99"
+            });
+
+            reviews.Add(new Review
+            {
+                Id = 6,
+                Title = "Smooth and elegant",
+                Content = "Smooth and elegant, with a lovely finish. A delightful whisky.",
+                Recommend = true,
+                WhiskyId = 6,
+                UserId = "d9ef08e4-4307-4a78-8b8c-afaea5d8a0d2"
+            });
+
+            reviews.Add(new Review
+            {
+                Id = 7,
+                Title = "Not bad",
+                Content = "Decent whisky, but nothing extraordinary. Would drink again though.",
+                Recommend = true,
+                WhiskyId = 7,
+                UserId = "a8909756-a101-47c5-8d52-085322ffa6e6"
+            });
+
+            reviews.Add(new Review
+            {
+                Id = 8,
+                Title = "Complex flavors",
+                Content = "Really enjoyed the complexity of flavors in this whisky. A must-try.",
+                Recommend = true,
+                WhiskyId = 8,
+                UserId = "bca5356a-d5d8-47d7-b314-e74901211b99"
+            });
+
+            reviews.Add(new Review
+            {
+                Id = 9,
+                Title = "Perfect for special occasions",
+                Content = "Save this one for special occasions. Truly a special whisky.",
+                Recommend = true,
+                WhiskyId = 9,
+                UserId = "d9ef08e4-4307-4a78-8b8c-afaea5d8a0d2"
+            });
+
+            reviews.Add(new Review
+            {
+                Id = 10,
+                Title = "Not impressed",
+                Content = "Was not impressed with this whisky. Expected more.",
+                Recommend = false,
+                WhiskyId = 10,
+                UserId = "a8909756-a101-47c5-8d52-085322ffa6e6"
+            });
+
+            return reviews.ToArray();
+        }
+    }
+}
---
+++ TheWhiskyRealm.Infrastructure/Data/Migrations/20240321053836_SeedReviews.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheWhiskyRealm.Infrastructure.Data
+{
+    public partial class SeedReviews : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Reviews",
+                columns: new[] { "Id", "Content", "Recommend", "Title", "UserId", "WhiskyId" },
+                values: new object[,]
+                {
+                    { 1, "This whisky has an amazing taste profile, rich and complex.", true, "Fantastic flavor!", "a8909756-a101-47c5-8d52-085322ffa6e6", 1 },
+                    { 2, "Really enjoyed sipping on this whisky, smooth with a nice finish.", true, "Smooth and enjoyable", "bca5356a-d5d8-47d7-b314-e74901211b99", 2 },
+                    { 3, "This whisky is unbeatable. Smooth and easy to drink.", true, "Great!", "d9ef08e4-4307-4a78-8b8c-afaea5d8a0d2", 3 },
+                    { 4, "Expected more from this whisky. Found it lacking in flavor.", false, "Disappointing", "a8909756-a101-47c5-8d52-085322ffa6e6", 4 },
+                    { 5, "This whisky is a real treat for the senses. Highly recommended.", true, "A real treat", "bca5356a-d5d8-47d7-b314-e74901211b99", 5 },
+                    { 6, "Smooth and elegant, with a lovely finish. A delightful whisky.", true, "Smooth and elegant", "d9ef08e4-4307-4a78-8b8c-afaea5d8a0d2", 6 },
+                    { 7, "Decent whisky, but nothing extraordinary. Would drink again though.", true, "Not bad", "a8909756-a101-47c5-8d52-085322ffa6e6", 7 },
+                    { 8, "Really enjoyed the complexity of flavors in this whisky. A must-try.", true, "Complex flavors", "bca5356a-d5d8-47d7-b314-e74901211b99", 8 },
+                    { 9, "Save this one for special occasions. Truly a special whisky.", true, "Perfect for special occasions", "d9ef08e4-4307-4a78-8b8c-afaea5d8a0d2", 9 },
+                    { 10, "Was not impressed with this whisky. Expected more.", false, "Not impressed", "a8909756-a101-47c5-8d52-085322ffa6e6", 10 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "Id",
+                keyValue: 10);
+        }
+    }
+}
