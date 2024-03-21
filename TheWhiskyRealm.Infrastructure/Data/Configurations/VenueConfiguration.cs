5cbd8bf134a9ea73f863fd6524796004ae233100
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-03-21 05:52:48 +0200

    Seeded Venues

---
+++ TheWhiskyRealm.Infrastructure/Data/Configurations/VenueConfiguration.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TheWhiskyRealm.Infrastructure.Data.Models;
+using static TheWhiskyRealm.Infrastructure.Constants.VenueDataConstants;
+
+namespace TheWhiskyRealm.Infrastructure.Data.Configurations;
+
+public class VenueConfiguration : IEntityTypeConfiguration<Venue>
+{
+    public void Configure(EntityTypeBuilder<Venue> builder)
+    {
+        builder
+            .HasCheckConstraint("CK_Capacity_Min", $"[Capacity] >= {MinCapacity}")
+            .HasCheckConstraint("CK_Capacity_Max", $"[Capacity] <= {MaxCapacity}");
+
+        builder.HasData(GenerateVenues());
+    }
+
+    private Venue[] GenerateVenues()
+    {
+        ICollection<Venue> venues = new HashSet<Venue>();
+
+        venues.Add(new Venue
+        {
+            Id = 1,
+            Name = "Masterpiece Whisky Bar",
+            CityId = 1,
+            Capacity = 7,
+        });
+
+        venues.Add(new Venue
+        {
+            Id = 2,
+            Name = "Bar Caldo",
+            CityId = 1,
+            Capacity = 6,
+        });
+
+        venues.Add(new Venue
+        {
+            Id = 4,
+            Name = "Hotel Marinela",
+            CityId = 1,
+            Capacity = 100,
+        });
+
+        venues.Add(new Venue
+        {
+            Id = 5,
+            Name = "Bar Sandaka",
+            CityId = 2,
+            Capacity = 3,
+        });
+
+        venues.Add(new Venue
+        {
+            Id = 6,
+            Name = "The Whisky Library",
+            CityId = 2,
+            Capacity = 10,
+        });
+
+        venues.Add(new Venue
+        {
+            Id = 7,
+            Name = "Hotel Imperial",
+            CityId = 2,
+            Capacity = 40,
+        });
+
+        venues.Add(new Venue
+        {
+            Id = 8,
+            Name = "Tasting Room",
+            CityId = 3,
+            Capacity = 10,
+        });
+
+        return venues.ToArray();
+    }
+}
---
+++ TheWhiskyRealm.Infrastructure/Data/Migrations/20240321035124_VenuesSeed.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheWhiskyRealm.Infrastructure.Data
+{
+    public partial class VenuesSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Venues",
+                columns: new[] { "Id", "Capacity", "CityId", "Name" },
+                values: new object[,]
+                {
+                    { 1, 7, 1, "Masterpiece Whisky Bar" },
+                    { 2, 6, 1, "Bar Caldo" },
+                    { 4, 100, 1, "Hotel Marinela" },
+                    { 5, 3, 2, "Bar Sandaka" },
+                    { 6, 10, 2, "The Whisky Library" },
+                    { 7, 40, 2, "Hotel Imperial" },
+                    { 8, 10, 3, "Tasting Room" }
+                });
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Capacity_Max",
+                table: "Venues",
+                sql: "[Capacity] <= 100");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Capacity_Min",
+                table: "Venues",
+                sql: "[Capacity] >= 3");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Capacity_Max",
+                table: "Venues");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Capacity_Min",
+                table: "Venues");
+
+            migrationBuilder.DeleteData(
+                table: "Venues",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Venues",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Venues",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Venues",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Venues",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Venues",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Venues",
+                keyColumn: "Id",
+                keyValue: 8);
+        }
+    }
+}
