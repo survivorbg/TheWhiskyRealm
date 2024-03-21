ed4d55e515cb30b104a69561284fb5df05a51745
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-03-21 05:38:04 +0200

    Seeded Cities

---
+++ TheWhiskyRealm.Infrastructure/Data/Configurations/CityConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TheWhiskyRealm.Infrastructure.Data.Models;
+
+namespace TheWhiskyRealm.Infrastructure.Data.Configurations;
+
+public class CityConfiguration : IEntityTypeConfiguration<City>
+{
+    public void Configure(EntityTypeBuilder<City> builder)
+    {
+        builder.HasData(GenerateCities());
+    }
+
+    private City[] GenerateCities()
+    {
+        ICollection<City> cities = new HashSet<City>();
+
+        cities.Add(new City
+        {
+            Id = 1,
+            Name = "Sofia",
+            ZipCode = "1000",
+            CountryId = 11
+        });
+
+        cities.Add(new City
+        {
+            Id = 2,
+            Name = "Plovdiv",
+            ZipCode = "4000",
+            CountryId = 11
+        });
+
+        cities.Add(new City
+        {
+            Id = 3,
+            Name = "Varna",
+            ZipCode = "9000",
+            CountryId = 11
+        });
+
+
+        
+
+
+        return cities.ToArray();
+    }
+}
---
+++ TheWhiskyRealm.Infrastructure/Data/Migrations/20240321033655_CitiesSeed.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheWhiskyRealm.Infrastructure.Data
+{
+    public partial class CitiesSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "CountryId", "Name", "ZipCode" },
+                values: new object[] { 1, 11, "Sofia", "1000" });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "CountryId", "Name", "ZipCode" },
+                values: new object[] { 2, 11, "Plovdiv", "4000" });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "CountryId", "Name", "ZipCode" },
+                values: new object[] { 3, 11, "Varna", "9000" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
