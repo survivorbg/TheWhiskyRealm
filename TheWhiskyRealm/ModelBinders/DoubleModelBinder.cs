a5a028225c771e63fc7c8897b5e21a187779dc8e
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-03-24 17:18:38 +0200

    Added DoubleModelBuilder

---
+++ TheWhiskyRealm/ModelBinders/DoubleModelBinder.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Globalization;
+
+namespace TheWhiskyRealm.ModelBinders;
+
+public class DoubleModelBinder : IModelBinder
+{
+
+    public Task BindModelAsync(ModelBindingContext bindingContext)
+    {
+        ValueProviderResult valueResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+
+        if (valueResult != ValueProviderResult.None &&
+            !string.IsNullOrEmpty(valueResult.FirstValue))
+        {
+            double result = 0D;
+            bool success = false;
+            try
+            {
+                string strValue = valueResult.FirstValue.Trim();
+                strValue = strValue.Replace(".", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+                strValue = strValue.Replace(",", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+
+                result = Convert.ToDouble(strValue, CultureInfo.CurrentCulture);
+                success = true;
+            }
+            catch (FormatException fe)
+            {
+                bindingContext.ModelState.AddModelError(bindingContext.ModelName, fe, bindingContext.ModelMetadata);
+            }
+            if (success)
+            {
+                bindingContext.Result = ModelBindingResult.Success(result);
+            }
+        }
+        return Task.CompletedTask;
+    }
+}
