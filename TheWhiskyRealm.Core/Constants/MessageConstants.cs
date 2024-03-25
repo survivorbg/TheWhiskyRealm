119944f7dd042ec0a2d1065861a047f0bac689d1
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-03-25 22:00:23 +0200

    minor changes

--- TheWhiskyRealm.Core/Constants/MessageConstants.cs
+++ TheWhiskyRealm.Core/Constants/MessageConstants.cs
@@ -5,4 +5,6 @@ public static class MessageConstants
     public const string RequiredMessage = "The {0} field is required!";
     public const string LengthMessage = "The field {0} must be between {2} and {1} characters long!";
     public const string ValueMessage = "The field {0} value must be between {1} and {2}!";
+    public const string RecommendBoolMessage = "Please select whether you recommend this whisky or not.";
+    public const string BoolValidOption = "Please select a valid option.";
 }
--- TheWhiskyRealm.Core/Models/Whisky/DetailsWhiskyViewModel.cs
+++ TheWhiskyRealm.Core/Models/Whisky/DetailsWhiskyViewModel.cs
@@ -2,8 +2,6 @@
 
 public class DetailsWhiskyViewModel
 {
-
-
     public int Id { get; set; }
 
     public string Name { get; set; } = string.Empty;
