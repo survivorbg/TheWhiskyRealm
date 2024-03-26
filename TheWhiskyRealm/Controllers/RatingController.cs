3b24782384add5b264a88dce2c0765eb840ce62b
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-03-26 19:41:24 +0200

    Added RatingController

---
+++ TheWhiskyRealm/Controllers/RatingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheWhiskyRealm.Core.Contracts;
+
+namespace TheWhiskyRealm.Controllers
+{
+    public class RatingController : BaseController
+    {
+        private readonly IRatingService ratingService;
+
+        public RatingController(IRatingService ratingService)
+        {
+            this.ratingService = ratingService;
+        }
+
+    }
+}
