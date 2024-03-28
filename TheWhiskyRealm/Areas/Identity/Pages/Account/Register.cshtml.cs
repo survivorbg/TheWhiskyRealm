cb5d0ea7eb29f718c5d44da88cd2ec8da46dd934
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-03-28 23:53:44 +0200

    Cleaned comments from Login/Register

--- TheWhiskyRealm/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ TheWhiskyRealm/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -1,8 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-#nullable disable
-
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
