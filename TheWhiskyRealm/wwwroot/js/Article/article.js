e7e55f2934c9953451e8ed4a07a282001e1c2d4f
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-04-04 14:44:15 +0300

    Fixed deleting articles and comments

---
+++ TheWhiskyRealm/wwwroot/js/Article/article.js
@@ -0,0 +1,10 @@
+﻿function confirmDelete(articleId) {
+    if (confirm("Are you sure you want to delete this article?")) {
+        window.location.href = '/Article/Delete/' + articleId;
+    }
+}
+function confirmDeleteComment(commentId) {
+    if (confirm("Are you sure you want to delete this article?")) {
+        window.location.href = '/Comment/Delete/' + commentId;
+    }
+}
