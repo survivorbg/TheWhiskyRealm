51bfb4ef6577cd6a4e570edc9737012c5b9816f4
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-04-08 17:27:09 +0300

    User menu component added

---
+++ TheWhiskyRealm/Components/UserMenuComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheWhiskyRealm.Components;
+
+public class UserMenuComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        return await Task.FromResult<IViewComponentResult>(View());
+    }
+}
