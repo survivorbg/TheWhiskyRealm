80164380ca3051f31cb386cec00492fa7477938f
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-04-15 02:32:28 +0300

    Added Event service unit tests first part

---
+++ TheWhiskyRealm.Tests/Services/EventServiceUnitTests.cs
@@ -0,0 +1,303 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TheWhiskyRealm.Core.Contracts;
+using TheWhiskyRealm.Core.Models.AdminArea.Venue;
+using TheWhiskyRealm.Core.Models.Event;
+using TheWhiskyRealm.Core.Services;
+using TheWhiskyRealm.Infrastructure.Data;
+using TheWhiskyRealm.Infrastructure.Data.Common;
+using TheWhiskyRealm.Infrastructure.Data.Models;
+
+namespace TheWhiskyRealm.Tests.Services;
+
+[TestFixture]
+public class EventServiceUnitTests
+{
+    private TheWhiskyRealmDbContext dbContext;
+    private IRepository repository;
+    private IEventService service;
+
+    [SetUp]
+    public async Task Setup()
+    {
+        var options = new DbContextOptionsBuilder<TheWhiskyRealmDbContext>()
+            .UseInMemoryDatabase(databaseName: "TheWhiskyRealmInMemoryDb" + Guid.NewGuid().ToString())
+            .Options;
+
+        dbContext = new TheWhiskyRealmDbContext(options);
+        repository = new Repository(dbContext);
+        service = new EventService(repository);
+
+
+        var organiser = new ApplicationUser
+        {
+            Id = "TestUserId",
+            UserName = "TestUser"
+        };
+        var venue = new Venue
+        {
+            Id = 1,
+            Name = "Test Venue",
+            Capacity = 100,
+            CityId = 1
+        };
+        var ev = new Event
+        {
+            Id = 1,
+            Title = "Test Event",
+            Description = "Test Description",
+            OrganiserId = organiser.Id,
+            StartDate = DateTime.Now.AddDays(2),
+            EndDate = DateTime.Now.AddDays(3),
+            AvailableSpots = venue.Capacity,
+            Price = 100,
+            VenueId = venue.Id
+        };
+
+        var ev2 = new Event
+        {
+            Id = 2,
+            Title = "Test Event2",
+            Description = "Test Description2",
+            OrganiserId = organiser.Id,
+            StartDate = DateTime.Now.AddDays(-3),
+            EndDate = DateTime.Now.AddDays(-2),
+            AvailableSpots = venue.Capacity,
+            Price = 100,
+            VenueId = venue.Id
+        };
+
+        await dbContext.Users.AddAsync(organiser);
+        await dbContext.Venues.AddAsync(venue);
+        await dbContext.Events.AddAsync(ev);
+        await dbContext.SaveChangesAsync();
+    }
+
+    [TearDown]
+    public async Task Teardown()
+    {
+        await dbContext.Database.EnsureDeletedAsync();
+        await dbContext.DisposeAsync();
+    }
+
+    [Test]
+    public async Task AddEventAsync_ShouldAddEventToDatabase()
+    {
+        // Arrange
+        var start = DateTime.Now.ToString();
+        var end = DateTime.Now.AddDays(1).ToString();
+        var model = new EventAddViewModel
+        {
+            Title = "New Event",
+            Description = "New Description",
+            StartDate = start,
+            EndDate = end,
+            Price = 100,
+            VenueId = 1
+        };
+        var userId = "TestUserId";
+
+        // Act
+        await service.AddEventAsync(model, DateTime.Parse(start), DateTime.Parse(end), userId);
+
+        // Assert
+        var addedEvent = await dbContext.Events.FirstOrDefaultAsync(e => e.Title == model.Title);
+        Assert.IsNotNull(addedEvent);
+        Assert.AreEqual(model.Title, addedEvent.Title);
+        Assert.AreEqual(model.Description, addedEvent.Description);
+        Assert.AreEqual(DateTime.Parse(model.StartDate), addedEvent.StartDate);
+        Assert.AreEqual(DateTime.Parse(model.EndDate), addedEvent.EndDate);
+        Assert.AreEqual(model.Price, addedEvent.Price);
+        Assert.AreEqual(model.VenueId, addedEvent.VenueId);
+        Assert.AreEqual(userId, addedEvent.OrganiserId);
+    }
+
+    [Test]
+    public async Task DeleteEventAsync_ShouldDeleteEventFromDatabase()
+    {
+        // Arrange
+        var eventId = 1;
+
+        // Act
+        await service.DeleteEventAsync(eventId);
+
+        // Assert
+        var deletedEvent = await dbContext.Events.FirstOrDefaultAsync(e => e.Id == eventId);
+        Assert.IsNull(deletedEvent);
+    }
+
+    //TODO Add test when there are UserEvents with that eventId
+
+    [Test]
+    public async Task EditEventAsync_ShouldEditEventInDatabase()
+    {
+        // Arrange
+        var model = new EventEditViewModel
+        {
+            Id = 1,
+            Title = "Edited Test Event",
+            Description = "Edited Test Description",
+            StartDate = DateTime.Now.AddDays(1).ToString(),
+            EndDate = DateTime.Now.AddDays(2).ToString(),
+            Price = 200,
+            VenueId = 1
+        };
+
+        // Act
+        await service.EditEventAsync(model, DateTime.Parse(model.StartDate), DateTime.Parse(model.EndDate));
+
+        // Assert
+        var editedEvent = await dbContext.Events.FirstOrDefaultAsync(e => e.Id == model.Id);
+        Assert.IsNotNull(editedEvent);
+        Assert.AreEqual(model.Title, editedEvent.Title);
+        Assert.AreEqual(model.Description, editedEvent.Description);
+        Assert.AreEqual(DateTime.Parse(model.StartDate), editedEvent.StartDate);
+        Assert.AreEqual(DateTime.Parse(model.EndDate), editedEvent.EndDate);
+        Assert.AreEqual(model.Price, editedEvent.Price);
+        Assert.AreEqual(model.VenueId, editedEvent.VenueId);
+    }
+    [Test]
+    public async Task EventExistAsync_WithExistingId_ShouldReturnTrue()
+    {
+        // Arrange
+        var existingEventId = 1;
+
+        // Act
+        var eventExists = await service.EventExistAsync(existingEventId);
+
+        // Assert
+        Assert.IsTrue(eventExists);
+    }
+
+    [Test]
+    public async Task EventExistAsync_WithNonExistingId_ShouldReturnFalse()
+    {
+        // Arrange
+        var nonExistingEventId = 999;
+
+        // Act
+        var eventExists = await service.EventExistAsync(nonExistingEventId);
+
+        // Assert
+        Assert.IsFalse(eventExists);
+    }
+
+    [Test]
+    public async Task GetAllEventsAsync_ShouldReturnAllEvents()
+    {
+        // Arrange
+        var actualEvents = dbContext.Events.Select(e => new AllEventViewModel
+        {
+            AvailableSpots = e.AvailableSpots,
+            Id = e.Id,
+            Price = e.Price,
+            StartDate = e.StartDate.ToString("hh:mm dddd, dd MMMM yyyy"),
+            Title = e.Title,
+            VenueName = e.Venue.Name
+        }).ToList();
+
+        // Act
+        var allEvents = await service.GetAllEventsAsync();
+
+        // Assert
+        Assert.AreEqual(actualEvents.Count, allEvents.Count);
+        CollectionAssert.AreEqual(actualEvents.Select(e => e.Id), allEvents.Select(e => e.Id));
+        CollectionAssert.AreEqual(actualEvents.Select(e => e.Title), allEvents.Select(e => e.Title));
+        CollectionAssert.AreEqual(actualEvents.Select(e => e.StartDate), allEvents.Select(e => e.StartDate));
+        CollectionAssert.AreEqual(actualEvents.Select(e => e.Price), allEvents.Select(e => e.Price));
+        CollectionAssert.AreEqual(actualEvents.Select(e => e.VenueName), allEvents.Select(e => e.VenueName));
+        CollectionAssert.AreEqual(actualEvents.Select(e => e.AvailableSpots), allEvents.Select(e => e.AvailableSpots));
+    }
+    [Test]
+    public async Task GetAllEventsInVenueAsync_ShouldReturnAllEventsInVenue()
+    {
+        // Arrange
+        var venueId = 1;
+        var actualEvents = dbContext.Events
+            .Where(e => e.VenueId == venueId)
+            .Select(e => new EventViewModel
+            {
+                Id = e.Id,
+                Title = e.Title,
+                StartDate = e.StartDate,
+                EndDate = e.EndDate,
+                Price = e.Price,
+                AvailableSpots = e.AvailableSpots,
+                OrganiserId = e.OrganiserId,
+                JoinedUsers = e.UsersEvents.Count()
+            })
+            .ToList();
+
+        // Act
+        var allEventsInVenue = await service.GetAllEventsInVenueAsync(venueId);
+
+        // Assert
+        Assert.AreEqual(actualEvents.Count, allEventsInVenue.Count);
+        CollectionAssert.AreEqual(actualEvents.Select(e => e.Id), allEventsInVenue.Select(e => e.Id));
+        CollectionAssert.AreEqual(actualEvents.Select(e => e.Title), allEventsInVenue.Select(e => e.Title));
+        CollectionAssert.AreEqual(actualEvents.Select(e => e.StartDate), allEventsInVenue.Select(e => e.StartDate));
+        CollectionAssert.AreEqual(actualEvents.Select(e => e.EndDate), allEventsInVenue.Select(e => e.EndDate));
+        CollectionAssert.AreEqual(actualEvents.Select(e => e.Price), allEventsInVenue.Select(e => e.Price));
+        CollectionAssert.AreEqual(actualEvents.Select(e => e.AvailableSpots), allEventsInVenue.Select(e => e.AvailableSpots));
+        CollectionAssert.AreEqual(actualEvents.Select(e => e.OrganiserId), allEventsInVenue.Select(e => e.OrganiserId));
+        CollectionAssert.AreEqual(actualEvents.Select(e => e.JoinedUsers), allEventsInVenue.Select(e => e.JoinedUsers));
+    }
+    [Test]
+    public async Task GetAllEventsInVenueAsync_WithNonExistingVenueId_ShouldReturnEmpty()
+    {
+        // Arrange
+        var nonExistingVenueId = 999;
+
+        // Act
+        var allEventsInVenue = await service.GetAllEventsInVenueAsync(nonExistingVenueId);
+
+        // Assert
+        Assert.IsEmpty(allEventsInVenue);
+    }
+    [Test]
+    public async Task GetAllPastEventsAsync_ShouldReturnAllPastEvents()
+    {
+        // Arrange
+        var actualEvents = dbContext.Events
+            .Where(e => e.StartDate < DateTime.Now)
+            .Select(e => new AllEventViewModel
+            {
+                Id = e.Id,
+                Title = e.Title,
+                StartDate = e.StartDate.ToString("hh:mm dddd, dd MMMM yyyy"),
+                Price = e.Price,
+                VenueName = e.Venue.Name,
+                AvailableSpots = e.AvailableSpots
+            })
+            .ToList();
+
+        // Act
+        var allPastEvents = await service.GetAllPastEventsAsync();
+
+        // Assert
+        Assert.AreEqual(actualEvents.Count, allPastEvents.Count);
+        CollectionAssert.AreEqual(actualEvents.Select(e => e.Id), allPastEvents.Select(e => e.Id));
+        CollectionAssert.AreEqual(actualEvents.Select(e => e.Title), allPastEvents.Select(e => e.Title));
+        CollectionAssert.AreEqual(actualEvents.Select(e => e.StartDate), allPastEvents.Select(e => e.StartDate));
+        CollectionAssert.AreEqual(actualEvents.Select(e => e.Price), allPastEvents.Select(e => e.Price));
+        CollectionAssert.AreEqual(actualEvents.Select(e => e.VenueName), allPastEvents.Select(e => e.VenueName));
+        CollectionAssert.AreEqual(actualEvents.Select(e => e.AvailableSpots), allPastEvents.Select(e => e.AvailableSpots));
+    }
+
+    [Test]
+    public async Task GetAllPastEventsAsync_WithNoPastEvents_ShouldReturnEmpty()
+    {
+        // Arrange
+        var actualEvents = dbContext.Events
+            .Where(e => e.StartDate < DateTime.Now)
+            .ToList();
+        dbContext.Events.RemoveRange(actualEvents);
+        await dbContext.SaveChangesAsync();
+
+        // Act
+        var allPastEvents = await service.GetAllPastEventsAsync();
+
+        // Assert
+        Assert.IsEmpty(allPastEvents);
+    }
+
+}
