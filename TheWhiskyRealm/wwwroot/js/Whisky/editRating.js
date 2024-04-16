1b0e85f52a0e2b1d9df26226cf89cc2da8e34fcd
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-04-17 00:59:38 +0300

    Fixed edit rating js function and edited buttons

--- TheWhiskyRealm/wwwroot/js/Whisky/editRating.js
+++ TheWhiskyRealm/wwwroot/js/Whisky/editRating.js
@@ -1,14 +1,14 @@
 ﻿$(document).ready(function () {
-    $('#editRatingButton').click(function () {
+    var editRatingFunction = function () {
         var whiskyId = $(this).data('whisky-id');
         $.get('/Rating/Edit', { id: whiskyId }, function (data) {
             $('.user-rating').replaceWith(data);
             $('#editRatingButton').text('Cancel Edit');
             $('#editRatingButton').off('click').click(function () {
-                $('.rating-container').replaceWith('<div class="user-rating">...</div>');
-                $('#editRatingButton').text('Edit Rating');
-                $('#editRatingButton').off('click').click(editRatingFunction);
+                location.reload(); // презарежда страницата
             });
         });
-    });
+    };
+
+    $('#editRatingButton').click(editRatingFunction);
 });
