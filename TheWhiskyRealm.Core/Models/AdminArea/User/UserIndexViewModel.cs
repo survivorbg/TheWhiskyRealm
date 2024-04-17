d852ad139bfca8402d3e014d8837745461d5832a
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-04-17 07:09:45 +0300

    Minor fixes

--- TheWhiskyRealm.Core/Models/AdminArea/User/UserIndexViewModel.cs
+++ TheWhiskyRealm.Core/Models/AdminArea/User/UserIndexViewModel.cs
@@ -5,6 +5,6 @@ public class UserIndexViewModel
     public IEnumerable<UserViewModel> Users { get; set; } = new List<UserViewModel>();
     public int CurrentPage { get; set; }
     public int TotalPages { get; set; }
-    public int PageSize { get; set; } = 15;
+    public int PageSize { get; set; } = 10;
 }
 
--- TheWhiskyRealm.Core/Models/AdminArea/Venue/VenueIndexViewModel.cs
+++ TheWhiskyRealm.Core/Models/AdminArea/Venue/VenueIndexViewModel.cs
@@ -5,5 +5,5 @@ public class VenueIndexViewModel
     public IEnumerable<VenueViewModel> Venues { get; set; } = new List<VenueViewModel>();
     public int CurrentPage { get; set; }
     public int TotalPages { get; set; }
-    public int PageSize { get; set; } = 15;
+    public int PageSize { get; set; } = 10;
 }
