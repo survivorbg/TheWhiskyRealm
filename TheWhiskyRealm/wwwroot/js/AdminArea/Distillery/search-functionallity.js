b093e7c9d5798a74c856b2174b83ccef932f761c
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-04-17 22:04:23 +0300

    Added search functionallity in Distillery Index view

---
+++ TheWhiskyRealm/wwwroot/js/AdminArea/Distillery/search-functionallity.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    $('#searchButton').click(function () {
+        var searchValue = $('#searchInput').val().toLowerCase();
+
+        $('tbody tr').each(function () {
+            var name = $(this).find('td:eq(1)').text().toLowerCase();
+            var region = $(this).find('td:eq(2)').text().toLowerCase();
+            var country = $(this).find('td:eq(3)').text().toLowerCase();
+
+            if (name.includes(searchValue) || region.includes(searchValue) || country.includes(searchValue)) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+    });
+
+    $('#resetButton').click(function () {
+        $('#searchInput').val('');
+        $('tbody tr').show();
+    });
+});
