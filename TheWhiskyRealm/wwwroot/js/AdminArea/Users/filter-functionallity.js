98953004c8e61f351b5597aed29611e499a0c811
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-04-17 22:04:46 +0300

    Added filter  functionallity in User Index View

---
+++ TheWhiskyRealm/wwwroot/js/AdminArea/Users/filter-functionallity.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    $('#userRoleFilterForm .dropdown-item').click(function (e) {
+        e.preventDefault();
+        var role = $(this).data('role');
+        if (role === 'All') {
+            $('tbody tr').show();
+        } else {
+            $('tbody tr').hide().filter(function () {
+                return $(this).find('td:eq(2)').text().trim() === role;
+            }).show();
+        }
+    });
+
+    $('#userClearFilterButton').click(function () {
+        $('#userRoleFilterForm .dropdown-item[data-role="All"]').click();
+    });
+});
+
+$(document).ready(function () {
+    $('.dropdown-toggle').dropdown();
+});
